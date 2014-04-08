34b8889a3150bbd9d96e52e18fff81975746c20d
Gregg Caines <gregg@caines.ca>
2014-04-08 16:48:13 -0700

    improve testing and test coverage.

--- Makefile
+++ Makefile
@@ -1,25 +1,22 @@
 REPORTER = spec
 test:
-	@NODE_ENV=test ./node_modules/.bin/mocha -b --reporter $(REPORTER) --recursive
+	@NODE_ENV=test ./node_modules/.bin/mocha -b --reporter $(REPORTER)
 
-lib-cov:
-	./node_modules/jscoverage/bin/jscoverage lib lib-cov
+lint:
+	./node_modules/.bin/jshint ./test ./index.js
 
 test-cov:
-	@NODE_ENV=test NODE_PATH=lib ./node_modules/.bin/mocha \
-		--require blanket \
-		--reporter html-cov 1> coverage.html
+	$(MAKE) lint
+	@NODE_ENV=test ./node_modules/.bin/istanbul cover \
+	./node_modules/mocha/bin/_mocha -- -R spec
 
-test-coveralls:	lib-cov
-	$(MAKE) test REPORTER=spec
+test-coveralls:
 	echo TRAVIS_JOB_ID $(TRAVIS_JOB_ID)
-	@ONJSON_COVERAGE=1 $(MAKE) test REPORTER=mocha-lcov-reporter | ./node_modules/coveralls/bin/coveralls.js || true
-	rm -rf lib-cov
-
-coveralls:
-	$(MAKE) test REPORTER=spec
-	@NODE_ENV=test NODE_PATH=lib ./node_modules/.bin/mocha \
-		--require blanket \
-		--reporter mocha-lcov-reporter | ./node_modules/coveralls/bin/coveralls.js
+	$(MAKE) test
+	@NODE_ENV=test ./node_modules/.bin/istanbul cover \
+	./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && \
+		cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js || true
 
 .PHONY: test
+
+
--- test/onJson.js
+++ test/onJson.js
@@ -1,32 +1,12 @@
 var should = require('should');
 var factory = require('../index');
+var http = require('http');
+var connect = require('connect');
+var hottap = require('hottap').hottap;
+    var server;
 
 // TODO test bad schema, successful schema, json parse error
 describe("onJson", function(){
-  it ("sets onJson on the object", function(done){
-    var req = {};
-    var res = {};
-    var middleware = factory(function(req, res, info){ throw "there shouldn't be an error!";});
-    middleware(req, res, function(){
-      (typeof req.onJson).should.equal('function');
-      done();
-    });
-  });
-  it ("throws an error when called with 2+ params", function(done){
-    var req = {};
-    var res = {};
-    var middleware = factory(function(req, res, info){ throw "there shouldn't be an error!";});
-    middleware(req, res, function(){
-      try {
-        req.onJson({}, {}, function(err, obj){
-          err.should.equal('some error');
-        });
-      } catch(ex){
-        ex.should.equal('req.onJson() was called with the wrong number of properties.');
-        done();
-      }
-    });
-  });
   it ("sets the error param when there's an error", function(done){
     var res = {};
     var req = {
@@ -45,125 +25,244 @@ describe("onJson", function(){
       });
     });
   });
-  it ("sets an obj param when successful", function(done){
-    var res = {};
-    var req = {
-      on : function(type, cb){
-        switch(type){
-          case 'data' : return cb('{"asdf":"asdf"}');
-          case 'end' : return cb();
-        }
-      }
-    };
-    var middleware = factory(function(req, res, info){ throw "there shouldn't be an error!";});
-    middleware(req, res, function(){
-      req.onJson(function(err, obj){
-        obj.should.eql({asdf:"asdf"});
+  describe("when used on a real server", function(){
+    afterEach(function(done){
+      server.close(function(){
         done();
       });
     });
-  });
-  it ("responds with an error if json doesn't parse", function(done){
-    var res = {};
-    var req = {
-      on : function(type, cb){
-        switch(type){
-          case 'data' : return cb('{"age":37,}');
-          case 'end' : return cb();
-        }
-      }
-    };
-    var middleware = factory(function(req, res, info){ 
-                  info.reason.should.equal('invalid json.');
-                  info.detail.should.equal('{"age":37,}');
-                  done();
-    });
-    middleware(req, res, function(){
-      req.onJson(function(err, obj){
-        should.fail('should not get here');
+    it ("sets onJson on the object", function(done){
+      var app = connect();
+      var middleware = factory(function(req, res, info){ throw "there shouldn't be an error!";});
+      app.use(middleware);
+      app.use(function(req, res){
+        res.end((typeof req.onJson));
+      });
+      server = http.createServer(app).listen(3000, function(){
+        hottap("http://localhost:3000/asdf").request("POST", function(err, response){
+          should.not.exist(err);
+          response.body.should.equal('function');
+          done();
+        });
       });
     });
-  });
-  it ("responds with an error if input incorrectly has additional properties", function(done){
-    var resumeWasCalled = false;
-    var schema = {
-             "properties": {
-                "age": {
-                  "type": "number"
-                },
-                "name": {
-                  "type": "string"
-                }
-              },
-              "additionalProperties" : false
-            };
-    var req = {
-      on : function(type, cb){
-        switch(type){
-          case 'data' : return cb('{"age":37, "name":"GDizzle", "wrong" : "wrong"}');
-                          // above mistakenly sends an additional property when
-                          // additionalProperties is false
-          case 'end' : return cb();
+    it ("throws an error when called with 2+ params", function(done){
+      var app = connect();
+      var middleware = factory(function(req, res, info){ throw "there shouldn't be an error!";});
+      app.use(middleware);
+      app.use(function(req, res){
+        try {
+          req.onJson({}, {}, function(err, obj){
+            should.not.exist(err);
+          });
+        } catch(ex){
+          res.end(ex.message);
         }
-      },
-      resume : function(){
-        resumeWasCalled = true;
-      }
-    };
-    var res = {};
-    var middleware = factory(function(req, res, info){ 
-            info.reason.should.equal('json failed schema validation.');
-            info.detail[0].message.should.equal('Additional properties are not allowed');
-            // detail looks like this:
-            // [ { uri: 'urn:uuid:167293d9-3c95-493d-826e-1bfd4146a8b9#',
-            // schemaUri: 'urn:uuid:b7e07efd-fd80-4370-8206-9162f4c39cc9#',
-            // attribute: 'additionalProperties',
-            // message: 'Additional properties are not allowed',
-            // details: false } ]
-            resumeWasCalled = true;
-            done();
+      });
+      server = http.createServer(app).listen(3000, function(){
+        hottap("http://localhost:3000/asdf").request("POST", {"Content-Type" : "application/json"}, 
+             '{"subject":"blah"}', function(err, response){
+               should.not.exist(err);
+               response.body.should.equal('req.onJson() was called with the wrong number of properties.');
+               done();
+        });
+      });
+
     });
-    middleware(req, res, function(){
-      req.onJson(schema, function(err, obj){
-        should.fail('should not get here');
+    it ("sets an obj param when successful", function(done){
+      var app = connect();
+      var middleware = factory(function(req, res, info){
+        throw "there shouldn't be an error!";
+      });
+      app.use(middleware);
+      app.use(function(req, res){
+        req.onJson(function(err, obj){
+          should.not.exist(err);
+          res.end(JSON.stringify(obj));
+        });
+      });
+      server = http.createServer(app).listen(3000, function(){
+        hottap("http://localhost:3000/asdf").request("POST", {"Content-Type" : "application/json"}, 
+             '{"subject":"blah"}', function(err, response){
+               should.not.exist(err);
+               response.body.should.equal('{"subject":"blah"}');
+               done();
+        });
       });
     });
-  });
-  it ("throws errors when input doesn't match schema", function(done){
-    var schema = {
-             "properties": {
-                "age": {
-                  "type": "number"
-                },
-                "name": {
-                  "type": "string"
-                }
-              }
-            };
-    var req = {
-      on : function(type, cb){
-        switch(type){
-          case 'data' : return cb('{"age":"37", "name":"GDizzle"}');
-                        // mistakenly send age as a string
-          case 'end' : return cb();
-        }
-      }
-    };
-    var res = {};
-    var middleware = factory(function(req, res, info){ 
-            info.reason.should.equal('json failed schema validation.');
-            info.detail[0].details[0].should.equal('number');
-            // detail looks like this:
-            // [ { uri: 'urn:uuid:67ef53a9-1b09-48b1-b97d-fae313e4ee39#/age',
-            // schemaUri: 'urn:uuid:51edca59-120a-4486-a961-0ee2aa5c276b#/properties/age',
-            // attribute: 'type',
-            // message: 'Instance is not a required type',
-            // details: [ 'number' ] } ]
-            done();
+    it ("responds with an error if json doesn't parse", function(done){
+      var app = connect();
+      var middleware = factory(function(req, res, info){
+        should.exist(info);
+        res.end(JSON.stringify(info));
+      });
+      app.use(middleware);
+      app.use(function(req, res){
+        req.onJson(function(err, obj){
+          throw "should not get here!";
+        });
+      });
+      server = http.createServer(app).listen(3000, function(){
+        hottap("http://localhost:3000/asdf").request("POST", {"Content-Type" : "application/json"}, 
+             '{"subject":"blah"', function(err, response){
+               should.not.exist(err);
+              var obj = JSON.stringify({"reason" : "invalid json.",
+                                        "detail" : "{\"subject\":\"blah\"" });
+               response.body.should.equal(obj);
+               done();
+        });
+      });
     });
-    middleware(req, res, function(){
-      req.onJson(schema, function(err, obj){
-        should.fail('should not get here');
+    it ("responds with an error if input incorrectly has additional properties", function(done){
+      var schema = {
+               "properties": {
+                  "age": {
+                    "type": "number"
+                  },
+                  "name": {
+                    "type": "string"
+                  }
+                },
+                "additionalProperties" : false
+              };
+      var app = connect();
+      var middleware = factory(function(req, res, info){ 
+        should.exist(info);
+        res.end(JSON.stringify(info));
+      });
+      app.use(middleware);
+      app.use(function(req, res){
+        req.onJson(schema, function(err, obj){
+          throw "should not get here!";
+        });
+      });
+      server = http.createServer(app).listen(3000, function(){
+        hottap("http://localhost:3000/asdf").request("POST", {"Content-Type" : "application/json"}, 
+             '{"age":37, "name":"GDizzle", "wrong" : "wrong"}', function(err, response){
+              // above mistakenly sends an additional property when
+              // additionalProperties is false
+               should.not.exist(err);
+               var info = JSON.parse(response.body);
+               info.reason.should.equal('json failed schema validation.');
+               info.detail[0].message.should.equal('Additional properties are not allowed');
+               // detail looks like this:
+               // [ { uri: 'urn:uuid:167293d9-3c95-493d-826e-1bfd4146a8b9#',
+               // schemaUri: 'urn:uuid:b7e07efd-fd80-4370-8206-9162f4c39cc9#',
+               // attribute: 'additionalProperties',
+               // message: 'Additional properties are not allowed',
+               // details: false } ]
+               done();
+        });
+      });
+    });
+    it ("throws errors when input type doesn't match schema", function(done){
+      var schema = {
+               "properties": {
+                  "age": {
+                    "type": "number"
+                  },
+                  "name": {
+                    "type": "string"
+                  }
+                },
+                "additionalProperties" : false
+              };
+      var app = connect();
+      var middleware = factory(function(req, res, info){ 
+        should.exist(info);
+        res.end(JSON.stringify(info));
+      });
+      app.use(middleware);
+      app.use(function(req, res){
+        req.onJson(schema, function(err, obj){
+          throw "should not get here!";
+        });
+      });
+      server = http.createServer(app).listen(3000, function(){
+        hottap("http://localhost:3000/asdf").request("POST", {"Content-Type" : "application/json"}, 
+             '{"age":"37", "name":"GDizzle"}', function(err, response){
+              // mistakenly send age as a string
+               should.not.exist(err);
+               var info = JSON.parse(response.body);
+               info.reason.should.equal('json failed schema validation.');
+               info.detail[0].details[0].should.equal('number');
+               // detail looks like this:
+               // [ { uri: 'urn:uuid:67ef53a9-1b09-48b1-b97d-fae313e4ee39#/age',
+               // schemaUri: 'urn:uuid:51edca59-120a-4486-a961-0ee2aa5c276b#/properties/age',
+               // attribute: 'type',
+               // message: 'Instance is not a required type',
+               // details: [ 'number' ] } ]
+               done();
+        });
+      });
+    });
+    it ("errors on regex NON-matches with \\w characters", function(done){
+      var schema = {
+               "properties": {
+                  "emailAddress": {
+                    "type": "string",
+                    "pattern" : "^\\w+@\\w+\\.\\w+$"
+                  },
+                  "name": {
+                    "type": "string"
+                  }
+                },
+                "additionalProperties" : false
+              };
+      var app = connect();
+      var middleware = factory(function(req, res, info){ 
+        should.exist(info);
+        res.end(JSON.stringify(info));
+      });
+      app.use(middleware);
+      app.use(function(req, res){
+        req.onJson(schema, function(err, obj){
+          res.end(JSON.stringify(obj));
+        });
+      });
+      server = http.createServer(app).listen(3000, function(){
+        hottap("http://localhost:3000/asdf").request("POST", {"Content-Type" : "application/json"}, 
+             '{"name":"allen steven david foster", "emailAddress":"asdf@asdfcom"}', function(err, response){
+               // email address is missing a .
+               should.not.exist(err);
+               var body = JSON.parse(response.body);
+               body.detail[0].message.should.equal('String does not match pattern');
+               done();
+        });
+      });
+    });
+    it ("allows regex matches with \\w characters", function(done){
+      var schema = {
+               "properties": {
+                  "emailAddress": {
+                    "type": "string",
+                    "pattern" : "^\\w+@\\w+\\.\\w+$"
+                  },
+                  "name": {
+                    "type": "string"
+                  }
+                },
+                "additionalProperties" : false
+              };
+      var app = connect();
+      var middleware = factory(function(req, res, info){ 
+        should.exist(info);
+        res.end(JSON.stringify(info));
+      });
+      app.use(middleware);
+      app.use(function(req, res){
+        req.onJson(schema, function(err, obj){
+          res.end(JSON.stringify(obj));
+        });
+      });
+      server = http.createServer(app).listen(3000, function(){
+        hottap("http://localhost:3000/asdf").request("POST", {"Content-Type" : "application/json"}, 
+             '{"name":"allen steven david foster", "emailAddress":"asdf@asdf.com"}', function(err, response){
+               should.not.exist(err);
+               var body = JSON.parse(response.body);
+               body.emailAddress.should.equal("asdf@asdf.com");
+               done();
+        });
       });
     });
   });
